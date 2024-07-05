2c67b0546f0eb409c7065b1b261bfd2bc22a8b33
Rody Shekr <shekrrody@gmail.com>
2024-07-05 20:52:44 +0200

    didn't saved the js file

--- js/script.js
+++ js/script.js
@@ -35,7 +35,6 @@ function textSequence(i) {
 
     if (hero_words.length > i) {
         setTimeout(function() {
-            document.getElementById("hero-sequence").innerHTML = ``;
             document.getElementById("hero-sequence").innerHTML = hero_words[i];
             textSequence(++i);
         }, 1500); // 1 second (in milliseconds)
@@ -53,7 +52,6 @@ function textSequenceButtonDE(i) {
 
     if (contactUsButtonDE.length > i) {
         setTimeout(function() {
-            document.getElementById(`button-sequence-de`).innerHTML = ``;
             document.getElementById(`button-sequence-de`).innerHTML = contactUsButtonDE[i];
             textSequenceButtonDE(++i);
         }, 2000); // 1 second (in milliseconds)
@@ -71,7 +69,6 @@ function textSequenceButtonEN(i) {
 
     if (contactUsButtonEN.length > i) {
         setTimeout(function() {
-            document.getElementById(`button-sequence-en`).innerHTML = ``;
             document.getElementById(`button-sequence-en`).innerHTML = contactUsButtonEN[i];
             textSequenceButtonEN(++i);
         }, 2000); // 1 second (in milliseconds)
